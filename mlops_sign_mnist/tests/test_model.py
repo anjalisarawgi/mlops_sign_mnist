628744529e1c4916fd22b1dd9e143be1dfa237a5
Anas <66208926+the-anas@users.noreply.github.com>
2024-07-08 11:58:56 +0200

    Update test_model.py

--- mlops_sign_mnist/tests/test_model.py
+++ mlops_sign_mnist/tests/test_model.py
@@ -2,18 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../mlops_sign_mnist')))
 
-
-
-# FILE HANDELING SECTION TO BE DELETED AND IS ONLY FOR DEBUGGING
-current_directory = os.getcwd()
-print(f"Current working directory: {current_directory}")
-
-# List the contents of the current directory
-print("Directory contents:")
-for item in os.listdir(current_directory):
-    print(item)
-
-
 import torch
 from mlops_sign_mnist.models.model import SignLanguageMNISTModel
 
